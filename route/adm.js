b43833d10580d1e7b0b48f40bae451e47c6da542
isaias-silva <zackblackmangold@gmail.com>
2022-01-01 01:24:50 -0300

    delete and create

--- route/adm.js
+++ route/adm.js
@@ -5,7 +5,7 @@ const info=require('../src/data/info.json')
 const dados=require('../src/data/dados.json');
 const bodyParser=require('body-parser');
 const { create } = require('domain');
-
+const fs=require('fs')
 const color=['\u001b[31m',
  '\u001b[34m',
  '\u001b[0m',]
@@ -17,7 +17,7 @@ route.use(bodyParser.urlencoded({
     extended: true
   }));
   function savedata(){
-    fs.writeFileSync('../src/data/dados.json',JSON.stringify(dados),(x)=>{console.log('save and reload')})
+    fs.writeFileSync('./src/data/dados.json',JSON.stringify(dados),(x)=>{console.log('save and reload')})
 }
 route.get('/',(req,res)=>{
     res.render('admin.ejs',{acess:dados.length,data:dados})
@@ -33,6 +33,7 @@ route.post('/create',(req,res)=>{
     obj.id=parseInt(Math.random()*9999999)
     console.log(color[1]+`${req.session.user} create ${obj.name}`+color[2])
     dados.push(obj)
+    savedata()
 res.redirect('/admin')
 })
 
@@ -43,7 +44,7 @@ route.get('/del/:id',(req,res)=>{
             console.log(color[0]+`${req.session.user} delete ${data[i].name}`+color[2])
             
             data.splice(i,1)
-         
+            savedata()
             res.redirect('/admin')
         }
     }
