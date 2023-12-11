eb6d0f6e9001af1b6ce80341978710044b191539
isaias-silva <zackblackmangold@gmail.com>
2023-12-11 16:09:08 -0300

    poo

---
+++ src/app.ts
@@ -0,0 +1,93 @@
+import express, { NextFunction, Request, Response, Express } from 'express'
+import { config } from 'dotenv'
+import pino from 'pino'
+import logger from './utils/logger'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import session from 'express-session'
+import { HttpError } from './utils/HttpError'
+import pageController from './controllers/page.controller'
+import extractDomain from './middlewares/extract.domain'
+import authController from './controllers/auth.controller'
+config()
+
+
+export class App {
+    private port: string | number
+    private server?: Express
+
+    constructor(port: string | number) {
+        this.port = port
+    }
+    initServer() {
+        this.server = express()
+        this.defineConfigServer()
+        this.defineMiddlewares()
+        this.defineRoutes()
+        this.treatingErrors()
+        this.server.listen(this.port,()=>{
+            logger.info(`server running in port ${this.port}`)
+        })
+    }
+    private defineConfigServer() {
+        if (!this.server) {
+            return
+        }
+        this.server.use(cors())
+        this.server.set('view engine', 'ejs')
+
+        this.server.use(bodyParser.urlencoded({
+            extended: false
+
+        }));
+        this.server.use(bodyParser.json())
+
+        this.server.use(session({ secret: process.env.SECRET || 'jojo', resave: true, saveUninitialized: true }))
+
+        this.server.use(express.static('public'));
+    }
+
+    private defineMiddlewares() {
+        if (!this.server) {
+            return
+        }
+        this.server.use([extractDomain])
+    }
+    private defineRoutes() {
+        if (!this.server) {
+            return
+        }
+        this.server.use(pageController)
+        this.server.use('/auth', authController)
+    }
+
+    private treatingErrors() {
+        if (!this.server) {
+            return
+        }
+        this.server.use((req: Request, res: Response, next: NextFunction) => {
+            const error = new HttpError(404, 'not found')
+
+            next(error);
+        })
+
+        this.server.use((erro: HttpError, req: Request, res: Response, next: NextFunction) => {
+            logger.error(erro)
+            return res.render('erro.ejs', { erro: erro.status, msg: erro })
+
+        }
+        )
+
+
+
+    }
+}
+
+const port = process.env.PORT || 8080
+const app = express()
+
+
+
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,5 @@
-import express, { NextFunction, Request, Response } from 'express'
-import { config } from 'dotenv'
-import pino from 'pino'
-import logger from './utils/logger'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import session from 'express-session'
-import { HttpError } from './utils/HttpError'
-import pageController from './controllers/page.controller'
-import extractDomain from './middlewares/extract.domain'
-import authController from './controllers/auth.controller'
+import { config } from "dotenv";
+import { App } from "./app";
 config()
-
-const port = process.env.PORT || 8080
-const app = express()
-
-app.use(cors())
-app.set('view engine', 'ejs')
-
-app.use(bodyParser.urlencoded({
-    extended: false
-
-
-}));
-app.use(bodyParser.json())
-
-app.use(session({ secret: process.env.SECRET || 'jojo', resave: true, saveUninitialized: true }))
-
-app.use(express.static('public'));
-
-app.use([extractDomain])
-
-app.use(pageController)
-app.use('/auth', authController)
-app.use((req: Request, res: Response, next: NextFunction) => {
-    const erro = new HttpError(404, 'not found')
-
-    next(erro);
-})
-app.use((erro: HttpError, req: Request, res: Response, next: NextFunction) => {
-
-    return res.render('erro.ejs', { erro: erro.status, msg: erro })
-
-}
-)
-app.listen(port, () => {
-    logger.info(`server running in ${port}`, 'api')
-})
-
-
+const app = new App(process.env.PORT || 8080)
+app.initServer()
