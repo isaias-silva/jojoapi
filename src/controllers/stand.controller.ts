0c24e2997e126ed56b8b449b3ad2ab0f3848336e
isaias-silva <zackblackmangold@gmail.com>
2024-01-07 18:01:08 -0300

    fix: :zap:

--- src/controllers/stand.controller.ts
+++ src/controllers/stand.controller.ts
@@ -18,30 +18,66 @@ export class StandController implements Controller {
     }
 
     defineRoutes() {
-        this.router.get(['/get/:id','/get'], async (req: Request, res: Response) => {
+        this.router.get(['/get/:id', '/get'], async (req: Request, res: Response) => {
             try {
                 const response = await this.StandService.get(req.params.id)
                 res.send(response)
-                
+
             } catch (err: any) {
                 res.status(err.status || 500).render('error.ejs', { status: err.status || 500, msg: err.message || 'internal' })
 
             }
         })
+
+        this.router.post('/admin/edit/:id/update', isLoging, upload.single('img'), async (req: Request, res: Response) => {
+            try {
+           
+               
+                await this.StandService.update(req.params.id, req.body)
+          
+                res.redirect('/stands/admin')
+
+            } catch (err: any) {
+                res.status(err.status || 500).render('error.ejs', { status: err.status || 500, msg: err.message || 'internal' })
+
+            }
+        })
+        this.router.get('/admin/delete/:id', isLoging, async (req: Request, res: Response) => {
+            try {
+                await this.StandService.delete(req.params.id)
+                res.redirect('/stands/admin')
+
+            } catch (err: any) {
+                res.status(err.status || 500).render('error.ejs', { status: err.status || 500, msg: err.message || 'internal' })
+
+            }
+        })
+
         this.router.get('/guide', async (req: Request, res: Response) => {
             const data = await this.StandService.get()
 
             res.render('index.ejs', { key: "guide", data, session: req.session.user })
         })
+
+
         this.router.get('/admin', isLoging, async (req: Request, res: Response) => {
 
             const data = await this.StandService.get()
 
-            res.render('stands.ejs', { data, func: 'create', obj: null, session: req.session.user })
+            res.render('stands.ejs', { data, func: 'admin/create', obj: null, session: req.session.user })
         })
+     
+      
+        this.router.get('/admin/edit/:id', isLoging, async (req: Request, res: Response) => {
 
+            const [data, obj] = await Promise.all([this.StandService.get(), this.StandService.get(req.params.id)])
 
-        this.router.post('/create', isLoging, upload.single('img'), async (req: Request, res: Response) => {
+            res.render('stands.ejs', { data, func: `${req.params.id}/update`, obj, session: req.session.user })
+        })
+      
+      
+      
+        this.router.post('/admin/create', isLoging, upload.single('img'), async (req: Request, res: Response) => {
             try {
                 await this.StandService.create(req.body)
 
--- src/services/stands.service.ts
+++ src/services/stands.service.ts
@@ -1,9 +1,10 @@
 import db from "../db";
 import { Istand } from "../interfaces/interface.stand";
-import {Stand} from "../models/stand.model";
+import { Stand } from "../models/stand.model";
 import { HttpError } from "../utils/HttpError";
 import * as fs from 'fs'
 import * as path from 'path'
+import logger from "../utils/logger";
 export class StandService {
     private standModel = Stand
 
@@ -58,4 +59,69 @@ export class StandService {
             throw new HttpError(err?.status || 500, err?.message || 'internal')
         }
     }
+    async delete(id: string) {
+        try {
+            await db.sync()
+            if (!id) {
+                throw new HttpError(400, 'id is required')
+
+            }
+
+            const stand = await this.standModel.findByPk(id)
+            if (!stand) {
+                throw new HttpError(404, 'stand not found.')
+
+            }
+            await stand.destroy()
+
+            return { message: 'stand deleted' }
+        } catch (err: any) {
+            throw new HttpError(err?.status || 500, err?.message || 'internal')
+
+        }
+    }
+
+    async update(id: string, stand: Istand) {
+        try {
+            console.log(stand)
+            await db.sync();
+
+            const exists = await this.standModel.findByPk(id)
+
+            const formatStand = { ...stand }
+            if (!exists) {
+                throw new HttpError(404, 'stand dont exists')
+
+            }
+            try {
+                const file = fs.readFileSync(path.resolve('uploads', stand.name))
+
+                const img = path.resolve('public', 'assets', `${stand.name}.png`)
+
+                fs.writeFileSync(img, file)
+
+                formatStand.img = `/assets/${stand.name}.png`
+
+                fs.unlinkSync(path.resolve('uploads', stand.name))
+            } catch (err) {
+                logger.info('not file stand image')
+            }
+            logger.info('updating...')
+
+
+            const update = await exists.update(formatStand)
+            await update.save()
+
+            return {
+                message: `stand ${stand.name} updated`
+            }
+        } catch (err: any) {
+
+            throw new HttpError(err?.status || 500, err?.message || 'internal')
+        }
+
+
+    }
+
+
 }
